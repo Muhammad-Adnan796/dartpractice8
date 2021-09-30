01083f4c1d6836c1f7cedc363ebd44b7dc890106
Muhammad Adnan <79781004+Adnan-Swati@users.noreply.github.com>
2021-09-30 09:51:44 -0700

    Add files via upload

---
+++ dartpractice8.dart
@@ -0,0 +1,17 @@
+// void main() {
+  
+  
+// //   for (var i=1; i<=20; i++){
+// //     print ('${2} * ${i} = ${2*i}');
+// //   }
+// //   var a1 = {1:'adnan',2:'zeeshan',3:'adil'};
+// //   print (a1.keys);
+ 
+
+// //   var a1 = {1:'adnan',2:'zeeshan',3:'adil'};
+// //   print (a1.values);
+  
+// //    var a1 = {1:'adnan',2:'zeeshan',3:'adil'};
+// //   print (a1);
+  
+// }
